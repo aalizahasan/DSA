904ddd3dbd3decc466d0db8d66f8ee5a2ce55b6b
Syeda Aliza <syedaalizahassannaqvi@gmail.com>
2025-07-19 18:56:01 +0500

    cheated

---
+++ src/SearchInRotatedSortedArray33.java
@@ -0,0 +1,37 @@
+class Solution33 {
+    public int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            // Left side is sorted
+            if (nums[start] <= nums[mid]) {
+                if (target >= nums[start] && target < nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+            // Right side is sorted
+            else {
+                if (target > nums[mid] && target <= nums[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
+
+
+public class SearchInRotatedSortedArray33 {
+}
